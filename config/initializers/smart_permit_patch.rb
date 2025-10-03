2a888f62ec67777c17f03d75895046d31e022366
windy <yafei@dao42.com>
2025-10-03 13:27:28 +0800

    chore: add field_for smart permit

--- config/initializers/smart_permit_patch.rb
+++ config/initializers/smart_permit_patch.rb
@@ -34,7 +34,29 @@ module SmartPermitPatch
       end
     end
 
-    super(*smart_filters)
+    result = super(*smart_filters)
+
+    # Post-process: Handle hash-style nested attributes with any prefix (e.g., "new_0", "abc_1", "0")
+    filters.each do |filter|
+      next unless filter.is_a?(Hash)
+
+      filter.each do |key, permitted_attrs|
+        hash_param = self[key]
+        next unless permitted_attrs.is_a?(Array) && hash_param.respond_to?(:each)
+
+        # Permit each hash entry individually, supporting any key format
+        permitted_hash = {}
+        hash_param.each do |hash_key, hash_val|
+          if hash_val.is_a?(ActionController::Parameters)
+            permitted_hash[hash_key] = hash_val.permit(*permitted_attrs)
+          end
+        end
+
+        result[key] = ActionController::Parameters.new(permitted_hash).permit! unless permitted_hash.empty?
+      end
+    end
+
+    result
   end
 end
 
